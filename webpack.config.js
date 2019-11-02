4152173e6626f13980cd6d11e9dc9b4f4e9d1726
Montblanc159 <guillaume.zaugg@gmail.com>
2019-11-02 14:43:56 +0100

    hmr works on css and js

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack')
+const dev = process.env.NODE_ENV === "development"
+
+const config = {
+  mode: dev ? "development" : "production",
+  entry: './assets/javascripts/main.js',
+  output: {
+    filename: '[name].js',
+    publicPath: '/dist/',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js', '.html', '.scss']
+  },
+  devServer: {
+    stats: 'minimal'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          dev ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: (loader) => [
+                require('autoprefixer')(),
+              ]
+            }
+          },
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(woff2|eot|ttf|otf)(\?.*)?$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name].[hash:8].[ext]',
+              limit: 8192
+            }
+          },
+          {
+            loader: 'img-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+}
+
+module.exports = config
